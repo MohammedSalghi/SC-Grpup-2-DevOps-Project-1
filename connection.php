8d9a23ee5210682f6dbd5d001923040d9035f0c2
wangconghao-1 <wangconghao@graduate.utm.my>
2025-07-02 20:22:42 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,1 +1,14 @@
+<?php
+$servername = "db";
+$username = "root";  
+$password = "root"; 
+$dbname = "edoc"; 
+
+$database = new mysqli($servername, $username, $password, $dbname);
+
+if ($database->connect_error) {
+    die("Échec de la connexion : " . $database->connect_error);
+}
+?>
+
 
